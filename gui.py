56de18b0d795860aa9e0917c31430599b6497875
i5-PC <mcienkowski@gmail.com>
2023-09-24 16:20:14 +0200

    Add Item window created

--- gui.py
+++ gui.py
@@ -8,6 +8,18 @@ now = datetime.now()
 curent_date = date.today()
 current_time = now.strftime("%H:%M:%S")
 
+#add window
+label_add = sg.Text("Enter a parameters of item")
+input_id = sg.Input("", key='id')
+input_s_num = sg.Input("", key='serial')
+input_description = sg.Input("", key='description')
+input_data = sg.Input("", key='serial')
+
+#Add a params for input to make them shorter
+add_layout = [[label_add],[input_id, input_s_num],
+              [input_description, input_data]]
+
+# Main Window
 label = sg.Text("Press Add button to enter item.")
 add_button = sg.Button("Add", key='add')
 toprow = ["ID","SERIAL_NUM","ITEM_DESC","DATE"]
@@ -16,14 +28,28 @@ item_table = sg.Table(values=get_items(), headings=toprow,
                       key='itemtable', enable_events=True, expand_x=True,
                       expand_y=True, enable_click_events=True,
                       selected_row_colors='black on yellow')
+edit_button = sg.Button("Edit", key='edit')
+remove_button = sg.Button("Remove", key='remove')
+exit_button = sg.Button("EXIT", key='exit')
+export_csv = sg.Button("Export to csv", key='ecsv')
+export_xlsx = sg.Button("Export to excel", key='exlsx')
 
-layout = [[label], [add_button], [item_table]]
+# Layout for main window
+layout = [[label], [add_button],
+          [item_table],
+          [edit_button, remove_button, export_csv, export_xlsx],
+          [exit_button]]
 
 window = sg.Window("Inventory - app", layout=layout, size=(600,400))
+window_add = sg.Window("Adding Item", layout=add_layout, size=(300,300))
 
 while True:
     event, values = window.read()
     match event:
+        case 'add':
+            window_add.read()
+        case 'exit':
+            break
         case sg.WIN_CLOSED:
             break
 window.close()
